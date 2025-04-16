e20f7bfb6107b21f75f804387028a51874bfab81
Ashley Morris <ashley.morris@travelcounsellors.com>
2025-04-16 13:12:26 +0100

    (fix) Removed unnecessary print

--- internal/ui/initialisation/initialisation.go
+++ internal/ui/initialisation/initialisation.go
@@ -7,15 +7,11 @@ import (
 
 func Run() error {
 	p := tea.NewProgram(newInitModel())
-	finalModel, err := p.Run()
+	_, err := p.Run()
 	if err != nil {
 		fmt.Printf("There's been an error: %v", err)
 		return err
 	}
-
-	// Printing finalModel.View() keeps the final view on screen when exiting the program,
-	// otherwise it gets cleared
-	fmt.Println(finalModel.View())
 
 	return nil
 }
